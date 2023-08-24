fb41d018c90af08e76e1a3fa9021b934bbbd4bdd
chriskilelo <chriskilelo@gmail.com>
2023-08-24 14:35:26 +0300

    Created the 'book_copies' table migration.

---
+++ database/migrations/2023_08_24_112800_create_book_copies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_copies', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn');
+            $table->string('barcode');
+            $table->string('year_published');
+            $table->string('edition')->nullable(true);
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('publisher_id')->constrained('publishers');
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_copies');
+    }
+};
