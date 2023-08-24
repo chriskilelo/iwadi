da38330019717f74e3121597a211bc2e20b35ca2
chriskilelo <chriskilelo@gmail.com>
2023-08-24 14:25:55 +0300

    Created the 'members' table migration.

---
+++ database/migrations/2023_08_24_111933_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('membership_number');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->date('birth_date');
+            $table->string('phone_number')->nullable(true);
+            $table->string('email_address');
+            $table->string('password');
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
