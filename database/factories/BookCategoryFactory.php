7df4e00a2ea0257cd2ed9b711a9e4ec35363db7a
chriskilelo <chriskilelo@gmail.com>
2023-08-25 17:52:16 +0300

    Implemented the factory called 'BookCategoryFactory'.

---
+++ database/factories/BookCategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => mt_rand(1, 200),
+            'category_id' => mt_rand(1, 30),
+            'is_active' => $this->faker->boolean(80),
+        ];
+    }
+}
